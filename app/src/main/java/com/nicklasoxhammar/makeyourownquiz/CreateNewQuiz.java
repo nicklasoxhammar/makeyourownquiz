2e3a41fec73e3dd99145512b00f656b15a9144e3
nicklasoxhammar <nicklas.oxhammar@hotmail.com>
2018-02-01 16:46:25 +0100

    added buttons and new activity

---
+++ app/src/main/java/com/nicklasoxhammar/makeyourownquiz/CreateNewQuiz.java
@@ -0,0 +1,17 @@
+package com.nicklasoxhammar.makeyourownquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nick on 2018-02-01.
+ */
+
+public class CreateNewQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_quiz);
+    }
+}
--- app/src/main/java/com/nicklasoxhammar/makeyourownquiz/MainActivity.java
+++ app/src/main/java/com/nicklasoxhammar/makeyourownquiz/MainActivity.java
@@ -1,7 +1,9 @@
 package com.nicklasoxhammar.makeyourownquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void startNewQuiz(View view){
+
+        Intent intent = new Intent(MainActivity.this, CreateNewQuiz.class);
+        startActivity(intent);
+
     }
 }
